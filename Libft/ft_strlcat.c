1f916f1dfb7e3b101fe9d09cbc494209ac88d3c8
Jaimgreg Ternero <jternero@c2r6s6.42malaga.com>
2022-09-23 20:33:49 +0200

    third commit

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,13 +6,27 @@
 /*   By: jternero <jternero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 19:34:23 by jternero          #+#    #+#             */
-/*   Updated: 2022/09/23 20:07:33 by jternero         ###   ########.fr       */
+/*   Updated: 2022/09/23 20:30:15 by jternero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    
+	size_t	c;
+	size_t	d;
+
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	c = ft_strlen(dst);
+	d = 0;
+	while (src[d] != '\0' && c + 1 < dstsize)
+	{
+		dst[c] = src[d];
+		c++;
+		d++;
+	}
+	dst[c] = '\0';
+	return (ft_strlen(dst) + ft_strlen(&src[d]));
 }
