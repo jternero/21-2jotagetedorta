2735fb3b17a32ebe5b4d0298066c2ea8ea820d86
jternero <112439716+jternero@users.noreply.github.com>
2022-09-23 23:56:03 +0100

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -43,5 +43,7 @@ size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 //This function changes a uppercase letter into lowercase letter
 int		ft_tolower(int c);
+// This function compares at most the first n bytes of str1 and str2.
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 #endif
